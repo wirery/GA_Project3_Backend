59c2c8632460b3b1ca00a4d5df820f2c562da81f
wirery <wire_ryan@yahoo.com>
2021-07-08 09:04:17 -0400

    Coins schema created

---
+++ coin_api/models/coins.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+const coinSchema = mongoose.Schema({
+    country: { type: String, required: true },
+    img: { type: String, required: true },
+    year: { type: Number, required: true },
+    quantity: { type: Number, required: true },
+    grade: { type: Number, required: true },
+    value: { type: Number, required: true }
+
+})
+
+const Coin = mongoose.model('Coins', coinSchema)
+
+module.exports = Coin
